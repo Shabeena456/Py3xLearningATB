95472ca8a6e5d1c58cc9e0bfa37f7dfade71d789
Shabeena456 <shabeena4444@gmail.com>
2024-09-26 01:42:59 +0530

    new file added

---
+++ ex01_June/Ex_10062024/Lab030.py
@@ -0,0 +1,27 @@
+print(10 / 10)
+
+print("------------------")
+
+
+# Logical Operator - bool
+x = 10
+y = 20
+print(x > y)
+print(x < y)
+print("------------------")
+
+
+a = 10
+b = 10
+print(a >= b)  # 10 > 10 or 10 = 10
+print(a == b)  # 10 > 10 or 10 = 10
+print(not a)
+
+print("------------------")
+
+# Or Gate
+
+f = False
+t = True
+print(f and t)
+print(f or t) # Truth Table of or
---
+++ ex01_June/Ex_10062024/Lab035.py
@@ -0,0 +1,12 @@
+# Math
+import math
+a = math.pow(2,2)
+pi = math.pi
+print(a)
+print(pi)
+
+result = math.sin(90)
+print(result)
+
+result2 = math.cos(90)
+print(result2)
